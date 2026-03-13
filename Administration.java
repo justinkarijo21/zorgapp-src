c2d9c855836551df976acf7ce9579f2908ab5dcf
Justin Karijo <justinkarijo21@gmail.com>
2026-03-13 13:45:25 +0100

    Refactored to new medicatioon classs

--- Administration.java
+++ Administration.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * constructor to the data member 'currentUser'.
  * The patient data is available via the data member currentPatient.
  */
-class Administration {
+public class Administration {
     static final int STOP = 0;
     static final int VIEW = 1;
     static final int QUICKSELECT = 2; 
@@ -103,15 +103,15 @@ class Administration {
                     break;
                
                 case ADD_MEDICATION:
-                    addMedicationToPatient(scanner);
+                    Medication.addMedicationToPatient(scanner, currentPatient);
                     break;
                 
                 case EDIT_MEDICATION:
-                    EditMedication(scanner);
+                    Medication.EditMedication(scanner, currentPatient);
                     break;
 
                 case DELETE_MEDICATION:
-                    DeleteMed(scanner);
+                    Medication.DeleteMed(scanner, currentPatient);
                     break;
 
                 default:
@@ -190,66 +190,6 @@ class Administration {
                             
     }
 
-    void addMedicationToPatient (Scanner Meds) { // voegt medication toe aan een patient
-            System.out.println("\nAdding medication for: " + currentPatient.fullName());
-            
-            System.out.print("Enter medication name: ");
-            
-                Meds.nextLine(); 
-                String medName = Meds.nextLine();
-
-                if (!medName.trim().isEmpty()) {
-                    System.out.print("Enter dosage (example, 500mg, 2 tablets): "); //voegt dosage toe aan patient
-                    String dosage = Meds.nextLine();
-
-                if (!dosage.trim().isEmpty()) {
-                    String fullMed = medName + " (" + dosage + ")";
-                    currentPatient.addMedication(fullMed);
-                    System.out.println("Successfully added: " + fullMed);
-                } else {
-                    System.out.println("Error: Dosage cannot be empty");
-                    }   
-                } else {
-                    System.out.println("Error: Medication cannot be empty");
-                }
-    }
-
-    void EditMedication (Scanner Editmedication){ //past medicatie van patient aan
-        System.out.println("\nEditing medication for: " + currentPatient.fullName());
-
-            List<String> meds = currentPatient.medications;
-            if (meds.isEmpty()) {
-                System.out.println("No medication to edit.");
-                return;
-            }
-
-            for (int i = 0; i < meds.size(); i++) {
-                System.out.println((i + 1) + ". " + meds.get(i));
-            }
-
-            System.out.print("\nWhich number do you want to edit? ");
-            int index = Editmedication.nextInt() - 1;
-            Editmedication.nextLine(); 
-
-            if (index >= 0 && index < meds.size()) {
-                String currentFullMed = meds.get(index);
-                System.out.println("Current: " + currentFullMed);
-
-                System.out.print("Enter new name (press Enter to keep current): ");
-                String newName = Editmedication.nextLine();
-                
-                System.out.print("Enter new dosage (press Enter to keep current): ");
-                String newDosage = Editmedication.nextLine();
-
-                // Alleen aanpassen als er iets getypt is, anders behoud je de oude
-                if (!newName.isEmpty() || !newDosage.isEmpty()) {
-                    
-                    String updatedEntry = (newName.isEmpty() ? "OldName" : newName) + " (" + (newDosage.isEmpty() ? "OldDosage" : newDosage) + ")";
-                    currentPatient.medications.set(index, updatedEntry);
-                    System.out.println("Updated!");
-                }
-            }
-    }
 
     void editPatientData (Scanner edit) {
         System.out.println("\n--- Editing Patient ID: " + currentPatient.id + " (ID can't be changed, PRESS ENTER TO KEEP THE SAME INFO) ---");
@@ -286,36 +226,5 @@ class Administration {
             System.out.println("\n    Patient data succesfully updated!");
     }
 
-    void DeleteMed(Scanner scanner) {
-        System.out.println("\nDeleting medication for: " + currentPatient.fullName());
-
-        List<String> meds = currentPatient.medications;
-
-        if (meds.isEmpty()) {
-            System.out.println("No medication to delete.");
-            return;
-        }
-
-        // toon lijst
-        for (int i = 0; i < meds.size(); i++) {
-            System.out.println((i + 1) + ". " + meds.get(i));
-        }
-
-        System.out.print("\nWhich number do you want to delete? ");
-
-        if (scanner.hasNextInt()) {
-            int index = scanner.nextInt() - 1;
-
-            if (index >= 0 && index < meds.size()) {
-                String removed = meds.remove(index);
-                System.out.println("Removed medication: " + removed);
-            } else {
-                System.out.println("Invalid number.");
-            }
-        } else {
-            System.out.println("Invalid input.");
-            scanner.next();
-                }
-                    }
 
 }
--- Medication.java
+++ Medication.java
@@ -1,2 +1,98 @@
+import java.util.Scanner;
+import java.util.List;
 
-import.java.util.Scanner
+public class Medication{
+
+    public static void addMedicationToPatient (Scanner Meds, Patient currentPatient) { // voegt medication toe aan een patient
+            System.out.println("\nAdding medication for: " + currentPatient.fullName());
+            
+            System.out.print("Enter medication name: ");
+            
+                Meds.nextLine(); 
+                String medName = Meds.nextLine();
+
+                if (!medName.trim().isEmpty()) {
+                    System.out.print("Enter dosage (example, 500mg, 2 tablets): "); //voegt dosage toe aan patient
+                    String dosage = Meds.nextLine();
+
+                if (!dosage.trim().isEmpty()) {
+                    String fullMed = medName + " (" + dosage + ")";
+                    currentPatient.addMedication(fullMed);
+                    System.out.println("Successfully added: " + fullMed);
+                } else {
+                    System.out.println("Error: Dosage cannot be empty");
+                    }   
+                } else {
+                    System.out.println("Error: Medication cannot be empty");
+                }
+    }
+
+    public static void EditMedication (Scanner Editmedication, Patient currentPatient){ //past medicatie van patient aan
+        System.out.println("\nEditing medication for: " + currentPatient.fullName());
+
+            List<String> meds = currentPatient.medications;
+            if (meds.isEmpty()) {
+                System.out.println("No medication to edit.");
+                return;
+            }
+
+            for (int i = 0; i < meds.size(); i++) {
+                System.out.println((i + 1) + ". " + meds.get(i));
+            }
+
+            System.out.print("\nWhich number do you want to edit? ");
+            int index = Editmedication.nextInt() - 1;
+            Editmedication.nextLine(); 
+
+            if (index >= 0 && index < meds.size()) {
+                String currentFullMed = meds.get(index);
+                System.out.println("Current: " + currentFullMed);
+
+                System.out.print("Enter new name (press Enter to keep current): ");
+                String newName = Editmedication.nextLine();
+                
+                System.out.print("Enter new dosage (press Enter to keep current): ");
+                String newDosage = Editmedication.nextLine();
+
+                // Alleen aanpassen als er iets getypt is, anders behoud je de oude
+                if (!newName.isEmpty() || !newDosage.isEmpty()) {
+                    
+                    String updatedEntry = (newName.isEmpty() ? "OldName" : newName) + " (" + (newDosage.isEmpty() ? "OldDosage" : newDosage) + ")";
+                    currentPatient.medications.set(index, updatedEntry);
+                    System.out.println("Updated!");
+                }
+            }
+    }
+
+    public static void DeleteMed(Scanner scanner, Patient currentPatient) {
+        System.out.println("\nDeleting medication for: " + currentPatient.fullName());
+
+        List<String> meds = currentPatient.medications;
+
+        if (meds.isEmpty()) {
+            System.out.println("No medication to delete.");
+            return;
+        }
+
+        // toon lijst
+        for (int i = 0; i < meds.size(); i++) {
+            System.out.println((i + 1) + ". " + meds.get(i));
+        }
+
+        System.out.print("\nWhich number do you want to delete? ");
+
+        if (scanner.hasNextInt()) {
+            int index = scanner.nextInt() - 1;
+
+            if (index >= 0 && index < meds.size()) {
+                String removed = meds.remove(index);
+                System.out.println("Removed medication: " + removed);
+            } else {
+                System.out.println("Invalid number.");
+            }
+        } else {
+            System.out.println("Invalid input.");
+            scanner.next();
+                }
+                    }
+}
