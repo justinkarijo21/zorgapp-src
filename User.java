b9fc845803e73ce8cad1c6797097e5520f4022bf
Justin Karijo <justinkarijo21@gmail.com>
2026-03-19 13:37:11 +0100

    optimized other user option

--- User.java
+++ User.java
@@ -43,7 +43,26 @@ public void selectOtherUser(Scanner scanner, Account account, Administration adm
         System.out.println("Available users:");
         for (User u : account.allUsers) {
             System.out.format("[%d] %s (%s)\n", u.getUserID(), u.getUserName(), u.getClass().getSimpleName());
-}
+        }
+        System.out.println("Enter the user ID to switch to (or 0 to cancel):");
+        if (scanner.hasNextInt()) {
+            int id = scanner.nextInt();
+            if (id == 0) {
+                System.out.println("Cancelled.");
+                return;
+            }
+            for (User u : account.allUsers) {
+                if (u.getUserID() == id) {
+                    admin.currentUser = u;
+                    System.out.println("Switched to user: " + u.getUserName() + " (" + u.getClass().getSimpleName() + ")");
+                    return;
+                }
+            }
+            System.out.println("Invalid user ID.");
+        } else {
+            System.out.println("Invalid input.");
+            scanner.next(); // consume invalid input
+        }
     }
         }
     
